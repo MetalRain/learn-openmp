5516d86dd3d5f6ec9f2c1eac52b8b93d357309a0
Otto Martikainen <martikainenotto@gmail.com>
2018-06-28 12:50:31 +0300

    Parallelize linked list using tasks

--- linked.c
+++ linked.c
@@ -21,19 +21,18 @@ int fib(int n) {
   int i, sum = 0;
   if (n < 2) {
     return (n);
-  } else {
-    // Cut off parallelism after some point to avoid too many threads
-    if (n > FS - DEPTH){
-      #pragma omp parallel for num_threads(2) reduction(+:sum)
-      for(i = 1; i < 3; i++){
-        sum += fib(n - i);
-      }
-      return sum;
-    } else {
-      sum = fib(n - 1) + fib(n - 2);
+  }
+  // Cut off parallelism after some point to avoid too many threads
+  if (n > FS - DEPTH){
+    #pragma omp parallel for num_threads(2) reduction(+:sum)
+    for(i = 1; i < 3; i++){
+      sum += fib(n - i);
     }
     return sum;
+  } else {
+    sum = fib(n - 1) + fib(n - 2);
   }
+  return sum;
 }
 
 void processwork(struct node* p) 
@@ -64,38 +63,43 @@ struct node* init_list(struct node* p) {
 }
 
 int main(int argc, char *argv[]) {
-     double start, end;
-     struct node *p=NULL;
-     struct node *temp=NULL;
-     struct node *head=NULL;
+  double start, end;
+  struct node *p=NULL;
+  struct node *temp=NULL;
+  struct node *head=NULL;
      
-   printf("Process linked list\n");
-     printf("  Each linked list node will be processed by function 'processwork()'\n");
-     printf("  Each ll node will compute %d fibonacci numbers beginning with %d\n",N,FS);      
+  printf("Process linked list\n");
+  printf("  Each linked list node will be processed by function 'processwork()'\n");
+  printf("  Each ll node will compute %d fibonacci numbers beginning with %d\n",N,FS);      
  
-     p = init_list(p);
-     head = p;
+  p = init_list(p);
+  head = p;
 
-     start = omp_get_wtime();
-     {
+  start = omp_get_wtime();
+  #pragma omp parallel
+  {
+    #pragma omp single
+    {
+      struct node * p = head;
+      while (p != NULL) {
+        #pragma omp task firstprivate(p)
+        processwork(p);
+        p = p->next;
+      }
+    }
+  }
 
-        while (p != NULL) {
-       processwork(p);
-       p = p->next;
-        }
-     }
+  end = omp_get_wtime();
+  p = head;
+  while (p != NULL) {
+    printf("%d : %d\n",p->data, p->fibdata);
+    temp = p->next;
+    free (p);
+    p = temp;
+  }  
+  free (p);
 
-     end = omp_get_wtime();
-     p = head;
-   while (p != NULL) {
-        printf("%d : %d\n",p->data, p->fibdata);
-        temp = p->next;
-        free (p);
-        p = temp;
-     }  
-   free (p);
+  printf("Compute Time: %f seconds\n", end - start);
 
-     printf("Compute Time: %f seconds\n", end - start);
-
-     return 0;
+  return 0;
 }
