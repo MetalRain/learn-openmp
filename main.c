42702bbf3826b4ad9c59008af27bbda94db83330
Otto Martikainen <martikainenotto@gmail.com>
2018-06-28 08:05:00 +0300

    Record actual number of threads

--- main.c
+++ main.c
@@ -2,33 +2,36 @@
 #include <omp.h>
 
 int main() {
-  int threads = 2;
-  long num_steps = 100000;
-  int steps_per_thread = num_steps / threads;
+  int requested_threads = 100;
+  int used_threads;
+  long num_steps = 10000000;
   double step_size = 1.0/(double)num_steps;
-  double sums[threads];
-  double total_sum = 0.0;
+  double sums[requested_threads];
+  double pi = 0.0;
   double elapsed = 0.0;
 
-  omp_set_num_threads(threads);
+  omp_set_num_threads(requested_threads);
   omp_get_wtime();
   #pragma omp parallel
   {
     int id = omp_get_thread_num();
-    int offset = steps_per_thread * id;
-    double x, sum = 0.0;
-
-    for(int i=offset; i < offset + steps_per_thread; i++){
-      x = (i + 0.5) * step_size;
-      sum = sum + 4.0 / (1.0 + x*x);
+    int threads = omp_get_num_threads();
+    double x;
+    sums[id] = 0.0;
+    if (id == 0){
+      used_threads = threads;
     }
-    sums[id] = sum * step_size;
+
+    for(int i=id; i < num_steps; i += threads){
+      x = (i + 0.5) * step_size;
+      sums[id] = sums[id] + 4.0 / (1.0 + x*x);
+    }
   }
   elapsed = omp_get_wtime();
 
-  for (int i=0; i < threads; i++) {
-    total_sum += sums[i];
+  for (int i=0; i < used_threads; i++) {
+    pi += sums[i] * step_size;
   }
 
-  printf("PI: %g took %gms using %d threads\n", total_sum, elapsed / 1000., threads);
+  printf("PI: %g took %gus using %d threads\n", pi, elapsed / 1000., used_threads);
 }
