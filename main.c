1a10f9398e076f186889cc7e7325a8c9c11fc1bf
Otto Martikainen <martikainenotto@gmail.com>
2018-06-28 09:02:22 +0300

    Use parallel for construct to simplify program

--- main.c
+++ main.c
@@ -13,7 +13,6 @@ int main(int argc, char **argv) {
     }
   }
 
-  int used_threads;
   long num_steps = 100000000;
   double step_size = 1.0/(double)num_steps;
   double pi = 0.0;
@@ -21,26 +20,13 @@ int main(int argc, char **argv) {
 
   omp_set_num_threads(requested_threads);
   omp_get_wtime();
-  #pragma omp parallel
-  {
-    int id = omp_get_thread_num();
-    int threads = omp_get_num_threads();
-    double x;
-    double sum = 0.0;
-    if (id == 0){
-      used_threads = threads;
-    }
-
-    for(int i=id; i < num_steps; i += threads){
-      x = (i + 0.5) * step_size;
-      sum += 4.0 / (1.0 + x*x);
-    }
-    sum *= step_size;
-
-    #pragma omp atomic
-    pi += sum;
+  #pragma omp parallel for reduction (+:pi)
+  for(int i=0; i < num_steps; i++){
+    double x = (i + 0.5) * step_size;
+    pi += 4.0 / (1.0 + x*x);
   }
+  pi /= num_steps;
   elapsed = omp_get_wtime();
 
-  printf("PI: %g took %gus using %d threads\n", pi, elapsed / 1000., used_threads);
+  printf("PI: %g took %g\n", pi, elapsed);
 }
